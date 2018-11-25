d797e6ec13c3ced650eae1b84f53315528ea6a80
Maciej Wódke <maciej@mwpro.pl>
2018-11-26 00:13:46 +0100

    Simple CosmosDB change feed

---
+++ 08/FunctionForCosmos/SumOrders.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionForCosmos
+{
+    public static class SumOrders
+    {
+        [FunctionName("SumOrders")]
+        public static void Run([CosmosDBTrigger(
+            databaseName: "Store",
+            collectionName: "Orders",
+            ConnectionStringSetting = "cosmosDbConnection",
+            LeaseCollectionName = "leases", CreateLeaseCollectionIfNotExists = true)]IReadOnlyList<Document> input, ILogger log)
+        {
+            if (input != null && input.Count > 0)
+            {
+                var completed = input.Where(x => x.GetPropertyValue<string>("state").Equals("completed"));
+                var sum = completed.Sum(x => x.GetPropertyValue<decimal>("total"));
+                log.LogInformation($"{completed.Count()} orders with sum of {sum}");
+            }
+        }
+    }
+}
